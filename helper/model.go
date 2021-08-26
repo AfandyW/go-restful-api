2256b907821d4e7b3d4214ec805b751b444f6009
afandy2000 <afandywibowo2000@gmail.com>
2021-08-26 10:31:37 +0800

    feat: add helper, check response category is not nil

--- helper/model.go
+++ helper/model.go
@@ -3,20 +3,38 @@ package helper
 import (
 	"go-restful-api/model/domain"
 	"go-restful-api/model/web"
+	"net/http"
 )
 
-func ToCategoryResponse(category domain.Category) web.CategoryResponse{
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
 	return web.CategoryResponse{
-		Id: category.Id,
+		Id:   category.Id,
 		Name: category.Name,
 	}
 }
 
-func ToCategoriesResponse(categories []domain.Category) []web.CategoryResponse{
+func ToCategoriesResponse(categories []domain.Category) []web.CategoryResponse {
 	var categoryResponses []web.CategoryResponse
 
 	for _, category := range categories {
 		categoryResponses = append(categoryResponses, ToCategoryResponse(category))
 	}
 	return categoryResponses
+}
+
+func ResponseCategoryNotNil(category web.CategoryResponse) (webResponse web.WebResponse) {
+	if category.Id == 0 {
+		webResponse = web.WebResponse{
+			Code:   http.StatusOK,
+			Status: "OK",
+			Data:   nil,
+		}
+	} else {
+		webResponse = web.WebResponse{
+			Code:   http.StatusOK,
+			Status: "OK",
+			Data:   category,
+		}
+	}
+	return
 }
